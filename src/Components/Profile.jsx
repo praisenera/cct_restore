abfc38064b8e80e0f166130876d2c787dd5acd6c
Aster255 <paaalcos@gmail.com>
2023-07-23 14:59:50 +0800

    connected backend to student profile

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import { auth, db } from "../config/firebaseconfig";
+import { collection, getDocs, query, where } from "firebase/firestore";
 function Profile() {
+  const [profile, setProfile] = useState({});
+  const [x, setX] = useState("");
+
+  const studentsRef = collection(db, "students");
+
+  const getProfile = async () => {
+    try {
+      const a = auth?.currentUser?.uid;
+      const q = query(studentsRef, where("userId", "==", a));
+      const b = await getDocs(q);
+      let c;
+      b.forEach((doc) => {
+        c = doc.data();
+        console.log(c);
+      });
+      setProfile(c);
+      // .filter((doc) => doc.userId == auth.currentUser.uid);
+      // setShipment(filteredData);gwt
+    } catch (err) {
+      if (auth.currentUser) {
+        alert("cannot access firebase server");
+      }
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getProfile();
+  }, [x]);
+
   return (
     <>
       <div className="col-lg-3">
@@ -19,11 +51,16 @@ function Profile() {
         <Container style={{ marginLeft: "250px", paddingTop: "50px" }}>
           <Row>
             <h1>Student Profile</h1>
-            <h5 className="pt-3">Status: Waiting for approval....</h5>
+            <h5 className="pt-3">
+              Status:{" "}
+              {profile?.status ? profile.status : "Waiting for approval...."}
+            </h5>
           </Row>
           <Row className="pt-4 ps-5">
             <div className="col-lg-6">
-              <h3>Name of Student</h3>
+              <h3>
+                Name of Student:{profile?.lastName},{profile?.firstName}
+              </h3>
               <p className="pt-3">Birthdate: </p>
               <p>Address: </p>
               <p>Email: </p>
